40eead75bc60a5c6d97f09699a2ff6c01e872516
Marnei Cardoso <prof.marneicardoso@gmail.com>
2021-11-10 19:33:22 -0300

    DOM - HTML e JS

---
+++ DOM - HTML e JS/js/script.js
@@ -0,0 +1,10 @@
+/*  Scripts: DOM - HTML e JavaScript
+    Autor: prof. Marnei Cardoso
+    Data: 09/11/2021
+    Versão: 1.1
+*/
+
+// Mudar valores com o .value
+function mudarTexto() {
+    document.getElementById('texto').value = "Novo texto";
+}
