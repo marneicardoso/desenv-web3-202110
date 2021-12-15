b0a47d4ad235af775c7b2ad072a13cb38afbf5fc
prof. Marnei Cardoso <prof.marneicardoso@gmail.com>
2021-12-14 22:38:08 -0300

    12.1 Agenda - Java Web com Spring Boot e DB

---
+++ 12.1 Agenda - Java Web com Spring Boot e DB/agenda_web/src/test/java/com/marneicardoso/agenda/AgendaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marneicardoso.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
