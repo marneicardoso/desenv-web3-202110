59e5b95d7894fe1dcbef13e979e0599fc452e2d7
prof. Marnei Cardoso <prof.marneicardoso@gmail.com>
2021-12-14 22:47:03 -0300

    12.1 Agenda - Java Web com Spring Boot e DB

--- 12.1 Agenda - Java Web com Spring Boot e DB/agenda_web/src/main/java/agenda/agenda_web/AgendaWebApplication.java
+++ 12.1 Agenda - Java Web com Spring Boot e DB/agenda_web/src/main/java/agenda/agenda_web/AgendaWebApplication.java
@@ -1,4 +1,4 @@
-package com.marneicardoso.agenda;
+package agenda.agenda_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,4 +9,5 @@ public class AgendaWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AgendaWebApplication.class, args);
 	}
+
 }
